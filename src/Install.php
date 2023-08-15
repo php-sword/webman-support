21a0080a1c4dd1fb470edca75c313691e946e5b7
Kyour.cn <kyour@outlook.com>
2023-08-15 12:05:58 +0800

    feat: 自动处理开发环境下IDE提示优化

---
+++ src/Install.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace sword;
+
+class Install
+{
+    const WEBMAN_PLUGIN = true;
+
+    /**
+     * @var array
+     */
+    protected static array $pathRelation = [
+    ];
+
+    /**
+     * Install
+     * @return void
+     */
+    public static function install()
+    {
+        static::installByRelation();
+    }
+
+    /**
+     * Uninstall
+     * @return void
+     */
+    public static function uninstall()
+    {
+        self::uninstallByRelation();
+    }
+
+    /**
+     * installByRelation
+     * @return void
+     */
+    public static function installByRelation()
+    {
+        if (!config('app.debug')) return;
+
+        $tp_ide_helper = base_path() . '/vendor/webman/think-orm/src/_ide_helper.php';
+        if (file_exists($tp_ide_helper)) {
+            //直接清空内容
+            file_put_contents($tp_ide_helper, '<?php');
+        }
+
+        $tp_orm_query = base_path() . 'vendor/topthink/think-orm/src/db/BaseQuery.php';
+        if (file_exists($tp_orm_query)) {
+            //替换find返回
+            $contents = file_get_contents($tp_orm_query);
+            $lines = explode("\n", $contents);
+            $targetLine = '    public function find($data = null)';
+
+            for ($i = 0; $i < count($lines); $i++) {
+                if (str_contains($lines[$i], $targetLine)) {
+                    $lines[$i - 2] = '     * @return static|null';
+                    break;
+                }
+            }
+
+            $newContents = implode("\n", $lines);
+            file_put_contents($tp_orm_query, $newContents);
+        }
+    }
+
+    /**
+     * uninstallByRelation
+     * @return void
+     */
+    public static function uninstallByRelation()
+    {
+    }
+
+}
--- src/helpers.php
+++ src/helpers.php
@@ -13,32 +13,3 @@ if (!function_exists('env')) {
         return $val === false ? $default : $val;
     }
 }
-
-//处理开发是IDE提示优化
-(function () {
-    if (!config('app.debug')) return;
-
-    $tp_ide_helper = base_path() . '/vendor/webman/think-orm/src/_ide_helper.php';
-    if (file_exists($tp_ide_helper)) {
-        //直接清空内容
-        file_put_contents($tp_ide_helper, '<?php');
-    }
-
-    $tp_orm_query = base_path() . 'vendor/topthink/think-orm/src/db/BaseQuery.php';
-    if (file_exists($tp_orm_query)) {
-        //替换find返回
-        $contents = file_get_contents($tp_orm_query);
-        $lines = explode("\n", $contents);
-        $targetLine = '    public function find($data = null)';
-
-        for ($i = 0; $i < count($lines); $i++) {
-            if (str_contains($lines[$i], $targetLine)) {
-                $lines[$i - 2] = '     * @return static|null';
-                break;
-            }
-        }
-
-        $newContents = implode("\n", $lines);
-        file_put_contents($tp_orm_query, $newContents);
-    }
-})();
